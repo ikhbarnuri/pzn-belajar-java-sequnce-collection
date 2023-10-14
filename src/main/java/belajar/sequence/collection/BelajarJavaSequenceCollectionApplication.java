17e5d8db5a516eabdc8c64195b7702b065c46b97
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2023-10-14 11:41:08 +0700

    initial commit

---
+++ src/main/java/belajar/sequence/collection/BelajarJavaSequenceCollectionApplication.java
@@ -0,0 +1,13 @@
+package belajar.sequence.collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaSequenceCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaSequenceCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/belajar/sequence/collection/BelajarJavaSequenceCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package belajar.sequence.collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaSequenceCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
